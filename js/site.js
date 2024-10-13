c03fc3367119e1e2ddb3252e64cdba96cf0937b6
Christian Hegner <300070366@fvtc.edu>
2024-10-12 22:13:41 -0500

    Added carousel

--- js/site.js
+++ js/site.js
@@ -18,3 +18,63 @@ if (isEvening == true) { //If it's evening, do this.
 
 localStorage.setItem('It\'s a secret to everybody.', '10th enemy has the bomb.')
 //Both are quotes from the original Legend of Zelda on NES
+
+
+//Week 6
+const urls = [
+    'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+].map(url => { (new Image()).src = url; return url })
+
+const images = document.querySelectorAll('#carousel img')
+
+let currentImage = 0
+const showImages = () => {
+    const offset = currentImage % urls.length
+    images.forEach((image, index) => {
+        const imageIndex = (index + offset + urls.length) % urls.length
+        image.src = urls[imageIndex]
+    })
+}
+
+const prev = document.querySelector('#prev')
+prev.addEventListener('click', event => {
+    if (currentImage >= 1) 
+        {
+            currentImage -= 1
+            showImages()
+        } else {
+            currentImage = 4
+            showImages()
+        }
+})
+const next = document.querySelector('#next')
+next.addEventListener('click', event => {
+    if (currentImage <= 4) 
+        {
+            currentImage += 1
+            showImages()
+        } else {
+            currentImage = 1
+            showImages()
+        }
+})
+
+setInterval(() => {
+    if (currentImage <= 4) 
+        {
+            currentImage += 1
+            console.log('currentImageSetInterval currentImage ID <= 4')
+            showImages()
+        } else {
+            currentImage = 1
+            console.log('currentImageSetInterval loop Pass')
+            showImages()
+        }
+}, 5000)
+showImages()
+
+//End Week 6
